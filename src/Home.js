4037c65b83bf25a0f495bf1cd40bd0a0eae01b39
emefaAngela <gelamengor@gmail.com>
2022-10-28 13:52:13 +0000

    fetching data and catching errors

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,47 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import BlogList from './BlogList';
 const Home = () => {
 
-    const [blogs,setBlogs]=useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs,setBlogs]=useState(null);
+    const [isPending, setisPending]=useState(true);
+    const[error,setError]=useState(null);
+
+    
+
+    
+    
+    useEffect(()=>{
+        setTimeout(() =>{
+        fetch('http://localhost:8000/blogss')
+            .then(res=>{
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource');
+                }
+                return res.json()
+            })
+            .then(data =>{
+                setError(null);
+                setBlogs(data);
+                setisPending(false);
+
+            })
+            .catch(err =>{
+                setisPending(false);
+                setError(err.message);
+            })
+        },1000);
+    },[]);
+    
     return (  
         <div className="home">
-            {blogs.map((blog)=>(
-                <div className='blog-preview' key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            {Error && <div>{error}</div>}
+            {isPending && <div>Loading ...</div>}
+            {blogs && <BlogList blogs={blogs} />}
             
+
         </div>
     );
-    }
+        }
+    
  
 export default Home;
