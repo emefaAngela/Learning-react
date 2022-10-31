db6dbc97e2f7ee63e8d1e85a670fc57aaf58e222
emefaAngela <gelamengor@gmail.com>
2022-10-31 21:43:53 +0000

    Reusing custom hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import{ BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
 
 function App() {
   // const title= "Welcome to the new blog";
@@ -12,9 +13,11 @@ function App() {
       <Navbar/>
       <div className='content'>
        <Routes>
-          <Route exact path='/' element={<Home/>}/>
+          <Route  path='/' element={<Home/>}/>
              
           <Route path='/create' element={<Create/>}/>
+
+          <Route path='/blogs/:id' element={<BlogDetails/>}/>
               
        </Routes>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = (props) => {
     const blogs=props.blogs;
     return ( 
         <div className="BlogList">
             {blogs.map((blog)=>(
                 <div className='blog-preview' key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    </Link>
              
                 </div>
             ))}
