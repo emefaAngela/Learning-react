acbbb4a3980776dca957b9fc08ad80fb12a80c16
emefaAngela <gelamengor@gmail.com>
2022-11-02 10:17:21 +0000

    making a post request

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,21 @@ const Create = () => {
     const[title,setTitle]= useState('');
     const[body,setBody]=useState('');
     const[author,setAuthor]=useState('mario');
+    const[isPending,setIspending]=useState(false);
 
     const handleSubmit=(e)=>{
         e.preventDefault();
         const blog={title,body,author};
+        setIspending(true);
+       
+        fetch('http://localhost:8000/blogs',{
+            method:'POST',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(blog)
+        }).then(()=>{
+            console.log('new blog added');
+            setIspending(false);
+        })
     }
     
     return ( 
@@ -37,7 +48,8 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                   </select>
-                  <button>Add Blog</button>
+                  {!isPending && <button>Add Blog</button>}
+                  {isPending && <button disabled>Adding Blog ...</button>}
             </form>
         </div>
      );
